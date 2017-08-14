123a91e786588223cc261150f7fbdf99401e94c0
That JS Dude <khan4019@gmail.com>
2017-08-13 20:40:47 -0500

    created pie chart

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 
 import Child from './components/Child';
 import SleepingFamily from './components/SleepingFamily';
+import Pie from './components/Pie';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Pie data={[5, 2, 7, 1, 1, 3, 4, 9]} width="200" height="200"/>
         <SleepingFamily/>
         <Child name="Sheldon"/>
         <Child name="Penny"/>
